0ec01181249979dafc6d53f46f72fbe92492e586
Vishal Dubey <hdubey64@gmail.com>
2023-07-24 16:48:03 +0530

    User Controller

---
+++ backend/controllers/user.controller.js
@@ -0,0 +1,9 @@
+const register = (req, res) => {};
+
+const logIn = (req, res) => {};
+
+const logOut = (req, res) => {};
+
+const getProfileDetails = (req, res) => {};
+
+export { register, logIn, logOut, getProfileDetails };
