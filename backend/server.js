f390f0cc0c8fc57f1b8882f9cac27d7f2a5f3771
Vishal Dubey <hdubey64@gmail.com>
2023-07-24 16:47:14 +0530

    Db Connection is establish

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,10 @@
+// import connectionToDB from "./config/dbConnection.js";
 import app from "./app.js";
+import connectionToDB from "./config/dbConnection.js";
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-   console.log(`App is running on http:local${PORT}`);
+app.listen(PORT, async () => {
+   await connectionToDB();
+   console.log(`App is running on http:localhost:${PORT}`);
 });
