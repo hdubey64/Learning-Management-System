0d4e277e6ba88eac50b275918987c66d057d2d14
Vishal Dubey <hdubey64@gmail.com>
2023-07-24 17:41:58 +0530

    Error Meddileware defines

---
+++ backend/middlewares/error.middleware.js
@@ -0,0 +1,11 @@
+const errorMiddleware = (err, req, res, next) => {
+   err.statusCode = err.status || 500;
+   err.message = err.message || "Something went wrong";
+   res.status(err.statusCode).json({
+      success: false,
+      message: err.message,
+      stack: err.stack,
+   });
+};
+
+export default errorMiddleware;
