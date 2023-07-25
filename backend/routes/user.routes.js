3ccb2e1a71035e8994c4f35e62b3556811ca8179
Vishal Dubey <hdubey64@gmail.com>
2023-07-25 13:08:09 +0530

    Middleware import

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -5,11 +5,12 @@ import {
    logOut,
    register,
 } from "../controllers/user.controller.js";
+import { isLoggedIn } from "../middlewares/auth.middleware.config.js";
 const router = Router();
 
 router.post("/ragister", register);
 router.post("/login", logIn);
 router.get("/logout", logOut);
-router.get("/me", getProfileDetails);
+router.get("/me", isLoggedIn, getProfileDetails);
 
 export default router;
