0dedccf8b1a1897085cc098fa2d7cc6513567ce6
Vishal Dubey <hdubey64@gmail.com>
2023-07-26 15:53:41 +0530

    Try catch added

--- backend/middlewares/auth.middleware.config.js
+++ backend/middlewares/auth.middleware.config.js
@@ -2,22 +2,26 @@ import AppError from "../utils/error.util.js";
 import jwt from "jsonwebtoken";
 
 const isLoggedIn = async (req, res, next) => {
-   const { token } = req.cookies;
+   try {
+      const { token } = req.cookies;
 
-   if (!token) {
-      return next(
-         new AppError(
-            "Aunauthenticated user is not logged in, Please login again",
-            401
-         )
-      );
+      if (!token) {
+         return next(
+            new AppError(
+               "Aunauthenticated user is not logged in, Please login again",
+               401
+            )
+         );
+      }
+
+      const user = await jwt.verify(token, process.env.SECRET_KEY);
+
+      req.user = user;
+
+      next();
+   } catch (error) {
+      return next(new AppError(error.message, 401));
    }
-
-   const userDeatils = await jwt.verify(token, process.env.SECRET_KEY);
-
-   req.user = userDetails;
-
-   next();
 };
 
 export { isLoggedIn };
