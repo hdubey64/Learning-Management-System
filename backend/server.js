711d5b4ec0d9663fb7aebdca2710dc0aedd89aad
Vishal Dubey <hdubey64@gmail.com>
2023-07-26 17:58:48 +0530

    Cloudinary config

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,14 @@
 import "dotenv/config";
 import app from "./app.js";
 import connectionToDB from "./config/dbConnection.js";
-
+import { v2 as cloudinary } from "cloudinary";
 const PORT = process.env.PORT || 5000;
+
+cloudinary.config({
+   cloud_name: process.env.CLOUDNIRAY_CLOUD_NAME,
+   api_key: process.env.CLOUDNIRAY_API_KEY,
+   api_secret: process.env.CLOUDNIRAY_API_SECRET,
+});
 
 app.listen(PORT, async () => {
    await connectionToDB();
