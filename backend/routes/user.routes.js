e234e692b833e2c7ca67e0d2726bbfded822bf13
Vishal Dubey <hdubey64@gmail.com>
2023-07-26 15:54:21 +0530

    fixed typo

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -8,7 +8,7 @@ import {
 import { isLoggedIn } from "../middlewares/auth.middleware.config.js";
 const router = Router();
 
-router.post("/ragister", register);
+router.post("/register", register);
 router.post("/login", logIn);
 router.get("/logout", logOut);
 router.get("/me", isLoggedIn, getProfileDetails);
