d7fbffdd7ee342387a6cc87097c4ed55431cf91e
Vishal Dubey <hdubey64@gmail.com>
2023-07-28 15:54:45 +0530

    URL Encode use

--- backend/app.js
+++ backend/app.js
@@ -8,6 +8,7 @@ import errorMiddleware from "./middlewares/error.middleware.js";
 const app = express();
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use(
    cors({
