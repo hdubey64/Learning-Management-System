103981d001e4fb9324eddb7afae5e13de0105f23
Vishal Dubey <hdubey64@gmail.com>
2023-07-28 15:59:08 +0530

    Forgot and Reset Password router

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -4,6 +4,8 @@ import {
    logIn,
    logOut,
    register,
+   forgotPassword,
+   resetPassword,
 } from "../controllers/user.controller.js";
 import { isLoggedIn } from "../middlewares/auth.middleware.config.js";
 import upload from "../middlewares/multer.middleware.js";
@@ -13,5 +15,7 @@ router.post("/register", upload.single("avatar"), register);
 router.post("/login", logIn);
 router.get("/logout", logOut);
 router.get("/me", isLoggedIn, getProfileDetails);
+router.post("/forgot-password", forgotPassword);
+router.post("/reset-password/:token", resetPassword);
 
 export default router;
