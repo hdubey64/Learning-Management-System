35e242e04a64e0d3a9ba4094ed10edeacff5037e
Vishal Dubey <hdubey64@gmail.com>
2023-07-31 11:25:34 +0530

    User Routes Variable router = userRouter

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -11,15 +11,15 @@ import {
 } from "../controllers/user.controller.js";
 import { isLoggedIn } from "../middlewares/auth.middleware.config.js";
 import upload from "../middlewares/multer.middleware.js";
-const router = Router();
+const userRouter = Router();
 
-router.post("/register", upload.single("avatar"), register);
-router.post("/login", logIn);
-router.get("/logout", logOut);
-router.get("/me", isLoggedIn, getProfileDetails);
-router.post("/forgot-password", forgotPassword);
-router.post("/reset-password/:token", resetPassword);
-router.post("/change-password", isLoggedIn, changePassword);
-router.put("/update/:id", isLoggedIn, upload.single("avatar"), updateUser);
+userRouter.post("/register", upload.single("avatar"), register);
+userRouter.post("/login", logIn);
+userRouter.get("/logout", logOut);
+userRouter.get("/me", isLoggedIn, getProfileDetails);
+userRouter.post("/forgot-password", forgotPassword);
+userRouter.post("/reset-password/:token", resetPassword);
+userRouter.post("/change-password", isLoggedIn, changePassword);
+userRouter.put("/update/:id", isLoggedIn, upload.single("avatar"), updateUser);
 
-export default router;
+export default userRouter;
