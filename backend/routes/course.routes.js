206cc8d210e46952a82dd44b5168012dfc1ed191
Vishal Dubey <hdubey64@gmail.com>
2023-07-31 15:52:23 +0530

    Some new routes are import

--- backend/routes/course.routes.js
+++ backend/routes/course.routes.js
@@ -1,14 +1,25 @@
 import { Router } from "express";
 import {
+   createCourse,
    getAllCourses,
    getLecturesByCousreId,
+   removeCourse,
+   updateCourse,
 } from "../controllers/course.controller.js";
 import { isLoggedIn } from "../middlewares/auth.middleware.config.js";
+import upload from "../middlewares/multer.middleware.js";
 
 const courseRouter = Router();
 
-courseRouter.route("/").get(getAllCourses);
+courseRouter
+   .route("/")
+   .get(getAllCourses)
+   .post(upload.single("thumbnail"), createCourse);
 
-courseRouter.route("/:id").get(isLoggedIn, getLecturesByCousreId);
+courseRouter
+   .route("/:id")
+   .get(isLoggedIn, getLecturesByCousreId)
+   .put(updateCourse)
+   .delete(removeCourse);
 
 export default courseRouter;
