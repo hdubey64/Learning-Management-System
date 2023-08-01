a36a75f9b9cb95b35967fb72bd7c1bc20e0a50cd
Vishal Dubey <hdubey64@gmail.com>
2023-08-01 15:38:07 +0530

    Course Remove, Lecture Add & Remove controller

--- backend/controllers/course.controller.js
+++ backend/controllers/course.controller.js
@@ -57,16 +57,20 @@ const createCourse = async (req, res, next) => {
       });
 
       if (req.file) {
-         const result = await cloudinary.v2.uploader.upload(req.file.path, {
-            folder: "lms",
-         });
-         console.log(result);
-         if (result) {
-            course.thumbnail.public_id = result.public_id;
-            course.thumbnail.secure_url = result.secure_url;
-         }
+         try {
+            const result = await cloudinary.v2.uploader.upload(req.file.path, {
+               folder: "lms",
+            });
+            console.log(result);
+            if (result) {
+               course.thumbnail.public_id = result.public_id;
+               course.thumbnail.secure_url = result.secure_url;
+            }
 
-         fs.rm(`uploads/${req.file.filename}`);
+            fs.rm(`uploads/${req.file.filename}`);
+         } catch (error) {
+            return next(new AppError(error.message, 500));
+         }
       }
       await course.save();
 
@@ -110,7 +114,100 @@ const updateCourse = async (req, res, next) => {
    }
 };
 
-const removeCourse = async (req, res, next) => {};
+const removeCourse = async (req, res, next) => {
+   try {
+      const { id } = req.params;
+      const course = await Course.findOne({ _id: id });
+
+      if (!course) {
+         return next(new AppError("Course not found", 500));
+      }
+      await Course.findByIdAndRemove(id);
+      return res.status(200).json({
+         success: true,
+         message: "Course deleted successfully",
+      });
+   } catch (error) {
+      return next(new AppError(error.message, 500));
+   }
+};
+
+const addLecturesByCourseId = async (req, res, next) => {
+   try {
+      const { title, description } = req.body;
+      const { id } = req.params;
+      const course = await Course.findOne({ _id: id });
+
+      if (!title || !description) {
+         return next(new AppError("All fields are required", 500));
+      }
+
+      if (!course) {
+         return next(new AppError("Course with given id Does not exist", 500));
+      }
+
+      const lectureData = {
+         title,
+         description,
+         lecture: {},
+      };
+
+      if (req.file) {
+         try {
+            const result = await cloudinary.v2.uploader.upload(req.file.path, {
+               folder: "lms",
+            });
+            console.log(result);
+            if (result) {
+               lectureData.lecture.public_id = result.public_id;
+               lectureData.lecture.secure_url = result.secure_url;
+            }
+
+            fs.rm(`uploads/${req.file.filename}`);
+         } catch (error) {
+            return next(new AppError(error.message, 500));
+         }
+      }
+      course.lectures.push(lectureData);
+
+      course.numbersOFLecture = course.lectures.length;
+
+      await course.save();
+
+      return res.status(200).json({
+         success: true,
+         message: "Lecture successfully added to the list of courses",
+         course,
+      });
+   } catch (error) {
+      return next(new AppError(error.message, 500));
+   }
+};
+
+const deleteLectureByCourseId = async (req, res, next) => {
+   try {
+      const { courseId, lectureId } = req.params;
+
+      const course = await Course.findOne({ _id: courseId });
+      console.log(course.lectures);
+      const lecture = course.lectures.filter(
+         (e) => e._id.toString() !== lectureId
+      );
+      // const lectureIdnew = lectures.map((e) => e.lecture);
+      course.lectures = lecture;
+
+      course.numbersOFLecture = lecture.length;
+      await course.save();
+
+      return res.status(200).json({
+         success: true,
+         course,
+      });
+   } catch (error) {
+      console.log(error);
+      return next(new AppError(error.message, 500));
+   }
+};
 
 export {
    getAllCourses,
@@ -118,4 +215,6 @@ export {
    createCourse,
    updateCourse,
    removeCourse,
+   addLecturesByCourseId,
+   deleteLectureByCourseId,
 };
